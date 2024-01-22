fceb37ff25ffd62245ec4cc8529d15a6a2c88961
jerome69400 <148350431+jerome69400@users.noreply.github.com>
2024-01-22 11:10:28 +0100

    vue exercie first commit

---
+++ vuejs/exercice_my_first_app/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import MyOptionComponent from './assets'
+</script>
+
+<template>
+  <!-- Monter les composants ici -->
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ vuejs/exercice_my_first_app/src/main.js
@@ -0,0 +1,11 @@
+import './assets/main.css'
+
+
+COMPLETER ET METTRE À JOUR LE CODE
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+
