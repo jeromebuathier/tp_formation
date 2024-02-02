83b95f334d0118e8a5009dd57334f1117d1cb0a5
jerome69400 <148350431+jerome69400@users.noreply.github.com>
2024-02-02 13:50:26 +0100

    Update files

---
+++ vuejs/app/src/App.vue
@@ -0,0 +1,56 @@
+<script >
+import {MainLayout, MainNav} from './components'
+
+export default {
+  name: 'App',
+  components: {
+    MainLayout,
+    MainNav
+  },
+  data() {
+    return {
+      userNavItemsArray : [
+        {
+          name: "Home",
+          link: "/",
+          target: "_self"
+        }
+      ],
+      navItemsArray : [
+        {
+          name: "Home",
+          link: "/",
+          target: "_self",
+          class: "link-body-emphasis"
+        },
+        {
+          name: "Products",
+          link: "/admin/products",
+          target: "_self",
+          class: "link-body-emphasis"
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <!-- Monter les composants ici -->
+  <main-layout>
+    <template #header>
+      <main-nav
+        :navItems="navItemsArray"
+        :userNavItems="userNavItemsArray"
+        :showUserNav="true"
+      />
+    </template>
+    
+    <router-view></router-view>
+
+  </main-layout>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vuejs/exercice_3/src/components/Layout/MainLayout.vue
@@ -0,0 +1,32 @@
+<script>
+    export default {
+        name: "MainLayout"
+    }
+</script>
+<template>
+    <!-- redéfinir les slots  -->
+        <!-- - nommer les slots -->
+        <!-- - rendu consitionnel de slots / expliquer -->
+        <!-- - injecter dans le slot Header -->
+    <div class="container">
+        <header
+            
+            class="p-3 mb-3 border-bottom"
+        >
+            <!-- - nommer le slot header -->
+            <slot
+            
+            ></slot>
+        </header>
+        <main>
+            <!-- -Doit on nommer le slot default ? C'est quoi le slot default ? -->
+            <slot></slot>
+        </main>
+        <footer>
+            <!-- - nommer le slot footer -->
+            <slot
+                  
+            ></slot>
+        </footer>
+    </div>
+</template>
